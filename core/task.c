6e28fe1e0d093e525374d1714bdf98444ff69519
ricegood <951227@gmail.com>
2018-05-06 20:54:42 +0900

    scheduling(roughly)

--- core/task.c
+++ core/task.c
@@ -32,6 +32,19 @@ int32u_t eos_destroy_task(eos_tcb_t *task) {
 }
 
 void eos_schedule() {
+	/* check current task */
+	if (_os_current_task == NULL)
+		_os_restore_context(_os_ready_queue[0]->ptrData);
+	else {
+		int32u_t stkPtr = _os_save_context();
+		if (stkPtr == NULL) {
+			/* function termination */
+			return;
+		} else {
+			_os_current_task->stkPtr = stkPtr; // save stkPtr to tcb
+			_os_restore_context(_os_ready_queue[0]->ptrData); // restore context
+		}
+	}
 }
 
 eos_tcb_t *eos_get_current_task() {
