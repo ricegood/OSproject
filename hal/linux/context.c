5145eb86232c7d0b436d4205b5b0618a37d9422a
ricegood <951227@gmail.com>
2018-05-06 19:22:53 +0900

    push %%eax

--- hal/linux/context.c
+++ hal/linux/context.c
@@ -49,6 +49,7 @@ void _os_restore_context(addr_t sp) {
 }
 
 addr_t _os_save_context() {
-  __asm__ __volatile__("push %%ecx\n\t");
+  __asm__ __volatile__ ("\
+    mov $0x0, %%esp;");
   //__asm__ __volatile__("pusha");
 }
