971f3c3ba49ff9fa07e73194c66806b9790d5cca
ricegood <951227@gmail.com>
2018-05-06 23:36:24 +0900

    (error)save context

--- hal/linux/context.c
+++ hal/linux/context.c
@@ -78,7 +78,7 @@ addr_t _os_save_context() {
     mov %%esp, %%eax;\
     push 12(%%esp);\
     push 12(%%esp);\
-    mov %%esp, %%ebx;\
+    mov 1(%%ebp), %%ebp;\
     leave;\
     ret;\
   resume_eip:"
