0f702dba0c961c9313ce141043c9bb7e8cd2bba8
ricegood <951227@gmail.com>
2018-06-18 00:41:46 +0900

    (roughly) send mq

--- core/comm.c
+++ core/comm.c
@@ -9,14 +9,55 @@
 
 void eos_init_mqueue(eos_mqueue_t *mq, void *queue_start, int16u_t queue_size, int8u_t msg_size, int8u_t queue_type) {
   mq->queue_start = (int8u_t *)queue_start;
+  mq->front = (int8u_t *)queue_start;
+  mq->rear = (int8u_t *)queue_start;
   mq->queue_size = queue_size;
   mq->msg_size = msg_size;
   mq->queue_type = queue_type;
-  eos_init_semaphore(&(mq -> putsem), queue_size, queue_type);
-  eos_init_semaphore(&(mq -> getsem), 0, queue_type);
+  eos_init_semaphore(&(mq->putsem), queue_size, queue_type);
+  eos_init_semaphore(&(mq->getsem), 0, queue_type);
 }
 
 int8u_t eos_send_message(eos_mqueue_t *mq, void *message, int32s_t timeout) {
+  if (eos_acquire_semaphore(&(mq->putsem), timeout) == 0) {
+    // Q. how about waiting here????
+    return;
+    /*
+    // fail to get semaphore
+    if (timeout == -1) return 0;
+    else {
+      // this is similar with semaphore implementation
+      // timeout == 0 : wait in wait queue
+      eos_tcb_t current_task = eos_get_current_task(); // get current task
+      current_task->state = WAITING; // change current state
+      if (mq->queue_type == 0) // FIFO
+        _os_add_node_tail(&(mq->wait_queue), &(current_task->node)); // add to wait queue
+      else (mq->queue_type == 1) // priority_based
+        _os_add_node_priority(&(mq->wait_queue), &(current_task->node)); // add to wait queue
+      eos_schedule(); // sleep this task
+
+      // timeout > 0 : wait until timeout end
+      if(timeout > 0) {
+        if(timer->tick >= wait_time) {
+          eos_restore_interrupt(_eflags_saved);
+          return 0;
+        }
+      }
+    }
+    */
+  }
+
+  // success to get semaphore
+  else {
+    memcpy(mq->rear, message, mq->msg_size); // copy the message on the rear
+    (mq->rear) += mq->msg_size; // update rear
+    // exceed the size???
+    /*
+    if(mq->rear >= mq->queue_start + mq->queue_size)
+      mq->rear = mq->queue_start;
+    */
+    eos_release_semaphore(&(mq->getsem)); // release semaphore
+  }
 }
 
 int8u_t eos_receive_message(eos_mqueue_t *mq, void *message, int32s_t timeout) {
