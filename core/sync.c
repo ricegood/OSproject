3bbbf0193a3b08136636e27ec01a27f57c542f06
ricegood <951227@gmail.com>
2018-06-17 21:53:17 +0900

    eos_init_semaphore

--- core/sync.c
+++ core/sync.c
@@ -1,14 +1,16 @@
 /********************************************************
  * Filename: core/sync.c
- * 
+ *
  * Author: wsyoo, RTOSLab. SNU.
- * 
+ *
  * Description: semaphore, condition variable management.
  ********************************************************/
 #include <core/eos.h>
 
 void eos_init_semaphore(eos_semaphore_t *sem, int32u_t initial_count, int8u_t queue_type) {
 	/* initialization */
+	sem->count = initial_count; // set initial count
+	sem->queue_type = queue_type; // set queue type
 }
 
 int32u_t eos_acquire_semaphore(eos_semaphore_t *sem, int32s_t timeout) {
