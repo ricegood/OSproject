e992b550a5ecb2d8ce5f5e6094b4ebf41b61af6e
ricegood <951227@gmail.com>
2018-06-18 15:10:11 +0900

    debug

--- user/work.c
+++ user/work.c
@@ -90,12 +90,12 @@ void task3() {
 }
 
 void eos_user_main() {
-    eos_create_task(&tcb1, stack1, STACK_SIZE, task1, NULL, 10);     // 태스크 1 생성
+    eos_create_task(&tcb1, stack1, STACK_SIZE, task1, NULL, 1);     // 태스크 1 생성
     eos_set_period(&tcb1, 2);
 
     eos_create_task(&tcb2, stack2, STACK_SIZE, task2, NULL, 10);    // 태스크 2 생성
     eos_set_period(&tcb2, 4);
 
-    eos_create_task(&tcb3, stack3, STACK_SIZE, task3, NULL, 50);    // 태스크 3 생성
+    eos_create_task(&tcb3, stack3, STACK_SIZE, task3, NULL, 10);    // 태스크 3 생성
     eos_set_period(&tcb3, 5);
 }
