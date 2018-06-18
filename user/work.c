4a9dacb2218d794d1ff147534def68b65f58d8eb
ricegood <951227@gmail.com>
2018-06-18 16:42:41 +0900

    original source

--- user/work.c
+++ user/work.c
@@ -56,7 +56,7 @@ void eos_user_main()
     eos_create_task(&tcb1, (addr_t)stack1, 8096, sender_task, NULL, 50);
     eos_create_task(&tcb2, (addr_t)stack2, 8096, receiver_task1, NULL, 10);
     eos_create_task(&tcb3, (addr_t)stack3, 8096, receiver_task2, NULL, 10);
-    eos_set_period(&tcb1, 3);
+    eos_set_period(&tcb1, 2);
     eos_set_period(&tcb2, 4);
     eos_set_period(&tcb3, 5);
 
