8dc2f0a3c1ee99c8b31384ad4fc7acdd5a52f955
ricegood <951227@gmail.com>
2018-06-18 15:05:57 +0900

    debug

--- user/work.c
+++ user/work.c
@@ -53,11 +53,11 @@ static void receiver_task2(void *arg)
 
 void eos_user_main()
 {
-    eos_create_task(&tcb1, (addr_t)stack1, 8096, sender_task, NULL, 50);
+    eos_create_task(&tcb1, (addr_t)stack1, 8096, sender_task, NULL, 10);
     eos_create_task(&tcb2, (addr_t)stack2, 8096, receiver_task1, NULL, 10);
     eos_create_task(&tcb3, (addr_t)stack3, 8096, receiver_task2, NULL, 10);
-    eos_set_period(&tcb1, 20);
-    eos_set_period(&tcb2, 40);
+    eos_set_period(&tcb1, 2);
+    eos_set_period(&tcb2, 4);
     eos_set_period(&tcb3, 5);
 
     eos_init_mqueue(&mq1, queue1, 5, 2, FIFO);
